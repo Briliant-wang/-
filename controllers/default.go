ab9c815a7af004b153d115ea181bdccb59947741
Briliant-wang <2438121546@qq.com>
2020-09-24 21:06:24 +0800

    3s

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"beeprojects2/models"
+	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego"
+	"io/ioutil"
 )
 
 type MainController struct {
@@ -9,12 +13,55 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-<<<<<<< HEAD
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "beego.geamil.com"
-=======
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "beego.geamil.com"
+    Username:=c.Ctx.Input .Query("user")
+	Password:=c.Ctx.Input .Query("psd")
+
+    if Username !="admin"||Password !="123456"{
+    	c.Ctx.ResponseWriter.Write([]byte("对不起数据校验错误"))//向前端请求数据
+		return
+	}else {
+		c.Ctx.ResponseWriter.Write([]byte("数据校验成功！！"))
+	};
 	c.Data["Website"] = "2438121546@qq.com"/*修改后的自己的邮箱*/
 	c.Data["Email"] = "www.baidu.com"/*修改后的网址为百度*/
->>>>>>> 289c93d... revied
 	c.TplName = "index.tpl"
+}
+
+
+
+
+
+//func(c *MainController)Post(){
+//	name :=c.Ctx.Request.FormValue("name")
+//	age :=c.Ctx.Request.FormValue("age")
+//	sex :=c.Ctx.Request.FormValue("sex")
+//	fmt.Println(name,age,sex)
+//	if name != "admin"&& age !="18"&& sex !="male"{
+//		c.Ctx.WriteString("数据校验失败!")
+//		return
+//	}else {
+//		c.Ctx.WriteString("数据校验成功！")
+//	}
+//}
+
+func (c *MainController)Post(){
+	//解析前端json的 格式
+	var Person models.Person
+	dataBytes,err:=ioutil.ReadAll(c.Ctx.Request.Body)
+	if err !=nil{
+		c.Ctx.WriteString("数据解析1失败")
+		return
+	}
+	err = json.Unmarshal(dataBytes,&Person)
+	if err !=nil{
+		fmt.Println(err.Error())
+		c.Ctx.WriteString("数据解2析失败")
+		return
+	}
+	fmt.Println("姓名",Person.Name)
+	fmt.Println("姓名",Person.Age)
+	fmt.Println("姓名",Person.Sex)
+	c.Ctx.WriteString("数据解析成功")
 }
--- models/person.go
+++ models/person.go
@@ -1,1 +1,10 @@
 package models
+
+/*
+ *人结构个体的定义
+ */
+type Person struct {
+	Name string
+	Age int
+	Sex string
+}
